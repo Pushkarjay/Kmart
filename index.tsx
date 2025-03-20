7a763fb8b198b98c81d27b30d7097e18f752487d
Pushkarjay Ajay <pushkarjay.ajay1@gmail.com>
2025-03-21 02:09:33 +0530

    h

---
+++ index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to Kmart</h1>
+      <p>Your one-stop marketplace for hostel goods</p>
+    </div>
+  )
+}
+
+export default HomePage
